f549865f073a055c20e42ab791009e63a9dbdc71
Unknown <shadoew@web.de>
2018-02-28 22:52:27 +0100

    Zusatz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        JFrame frame = new JFrame("JFrame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container contentPane = frame.getContentPane();
+
+        //Schließen Button
+        JButton closeButton = new JButton("Close");
+        contentPane.add(closeButton);
+
+        //Größe setzen
+        frame.setBounds(50, 50, 300, 200);
+        frame.setVisible(true);
     }
 }
