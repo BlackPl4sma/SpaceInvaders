7b2d032849eb29f2f03a07a3c74aeae89ca0a02e
Unknown <shadoew@web.de>
2018-03-01 01:20:26 +0100

    Menu und Background aus Image Vorbereitung + Resourcen Ordner

--- src/com/company/Commons.java
+++ src/com/company/Commons.java
@@ -2,8 +2,12 @@ package com.company;
 
 //Oft genutzte Werte kommen hier hin :)
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
 public interface Commons {
 
     public static final int BOARD_WIDTH = 900;
     public static final int BOARD_HEIGHT = 600;
+    public final BufferedImage menuBackground = ImageIO.read(this.getClass().getResource("backgroungMenu"))
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,50 @@
 package com.company;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
 public class Main implements Commons{
 
-    public static void main(String[] args) {
+
+
+    public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame("SpaceInvaders");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel menuPanel = new JPanel();
 
-        //Spielen Button
-        JButton playButton = new JButton("Spielen");
-        menuPanel.add(playButton);
-        frame.add(menuPanel);
-        //Größe setzen
+        //JFrame
         frame.setBounds(100, 100, BOARD_WIDTH, BOARD_HEIGHT);
         frame.setVisible(true);
-        //Hintergrund
+        frame.getBackground();
 
-        
+        //Menu Panel
+        JPanel menuPanel = new JPanel();
+        JButton playButton = new JButton("Spielen");
+        menuPanel.add(playButton);
 
+        //Menü Panel hinzufügen
+        frame.add(menuPanel);
+
+
+
+
+
+
+
+    }
+    //möglichkeit zum Einfügen eines normalen Bilds
+    class ImagePanel extends JComponent {
+        private Image image;
+        public ImagePanel(Image image) {
+            this.image = image;
+        }
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(image, 0, 0, this);
+        }
     }
 }
